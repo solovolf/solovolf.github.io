d905c11f33b541288fb5e09050278254d92088d2
袁康 <yuankang@yuankangdeMacBook-Pro.local>
2016-11-24 13:17:45 +0800

    添加静态页面

---
+++ article/js/index.js
@@ -0,0 +1,26 @@
+(function () {
+    //判断ie
+    if (!document.addEventListener) {
+        alert('此网页不支持低版本ie，请使用最新的chrome浏览器！')
+        return;
+    }
+
+    var instance = new Vue({
+        el: '.container',
+        data: function () {
+            return {
+                menuList: menuList,
+                artList: articleList.article
+            }
+        },
+        methods:{
+            changeArtList:function(msg,event){
+                this.artList=articleList[msg];
+            },
+            openUrl:function(msg,event){
+                console.log(event.currentTarget.getAttribute("data-url"));
+                window.open(msg)
+            }
+        }
+    })
+})()
