d0a835daf93bb87aa59ce6875ca4065f41d5ddc6
袁康 <yuankang@yuankangdeMacBook-Pro.local>
2016-11-24 13:52:17 +0800

    databug

--- article/js/data.js
+++ article/js/data.js
@@ -9,29 +9,29 @@ var menuList = [
 var articleList={
     article:[
         {name:'requestAnimationFrame动画算法',url:'http://www.zhangxinxu.com/wordpress/2013/09/css3-animation-requestanimationframe-tween-%E5%8A%A8%E7%94%BB%E7%AE%97%E6%B3%95/'},
-        {name:'什么是viewport，为啥需要viewport',type:'http://www.myexception.cn/mobile/428756.html'},
-        {name:'使用Flexible实现手淘H5页面的终端适配',type:'https://github.com/amfe/article/issues/17'},
-        {name:'移动端高清、多屏适配方案',type:'http://www.html-js.com/article/Mobile-terminal-H5-mobile-terminal-HD-multi-screen-adaptation-scheme%203041'},
-        {name:'H5 缓存机制浅析 - 移动端 Web 加载性能优化',type:'https://segmentfault.com/a/1190000004132566'},
-        {name:'让IE6/IE7/IE8浏览器支持CSS3属性',type:'http://www.zhangxinxu.com/wordpress/2010/04/%E8%AE%A9ie6ie7ie8%E6%B5%8F%E8%A7%88%E5%99%A8%E6%94%AF%E6%8C%81css3%E5%B1%9E%E6%80%A7/'},
+        {name:'什么是viewport，为啥需要viewport',url:'http://www.myexception.cn/mobile/428756.html'},
+        {name:'使用Flexible实现手淘H5页面的终端适配',url:'https://github.com/amfe/article/issues/17'},
+        {name:'移动端高清、多屏适配方案',url:'http://www.html-js.com/article/Mobile-terminal-H5-mobile-terminal-HD-multi-screen-adaptation-scheme%203041'},
+        {name:'H5 缓存机制浅析 - 移动端 Web 加载性能优化',url:'https://segmentfault.com/a/1190000004132566'},
+        {name:'让IE6/IE7/IE8浏览器支持CSS3属性',url:'http://www.zhangxinxu.com/wordpress/2010/04/%E8%AE%A9ie6ie7ie8%E6%B5%8F%E8%A7%88%E5%99%A8%E6%94%AF%E6%8C%81css3%E5%B1%9E%E6%80%A7/'},
     ],
     javascript:[
-        {name:'44个 Javascript 变态题解析 (上)',type:'http://xiaoyu2er.github.io/2016/06/06/44-js-puzzles-part1/'},
-        {name:'js 优化原则',type:'http://hax.iteye.com/blog/126859'},
-        {name:'详解js闭包',type:'https://segmentfault.com/a/1190000000652891'},
-        {name:'前端文本截断',type:'http://efe.baidu.com/blog/text-truncating/'},
+        {name:'44个 Javascript 变态题解析 (上)',url:'http://xiaoyu2er.github.io/2016/06/06/44-js-puzzles-part1/'},
+        {name:'js 优化原则',url:'http://hax.iteye.com/blog/126859'},
+        {name:'详解js闭包',url:'https://segmentfault.com/a/1190000000652891'},
+        {name:'前端文本截断',url:'http://efe.baidu.com/blog/text-truncating/'},
     ],
     protocol:[
-        {name:'shadowsocks简介',type:'http://vc2tea.com/whats-shadowsocks/'},
-        {name:'如何理解 TCP/IP, SPDY, WebSocket 三者之间的关系？',type:'https://www.zhihu.com/question/20097129'},
-        {name:'http协议详解',type:'http://www.cnblogs.com/li0803/archive/2008/11/03/1324746.html'},
+        {name:'shadowsocks简介',url:'http://vc2tea.com/whats-shadowsocks/'},
+        {name:'如何理解 TCP/IP, SPDY, WebSocket 三者之间的关系？',url:'https://www.zhihu.com/question/20097129'},
+        {name:'http协议详解',url:'http://www.cnblogs.com/li0803/archive/2008/11/03/1324746.html'},
     ],
     vuejs:[
         {name:'vuex2.0源码分析',url:'http://gold.xitu.io/post/58352aaf880741006cfd65af'},
         {name:'vue源码分析',url:'https://github.com/youngwind/blog'}
     ],
     browser:[
-        {name:'浏览器是怎样工作的',type:'http://ued.ctrip.com/blog/how-browsers-work-rendering-engine-html-parsing-series-ii.html'},
-        {name:'浏览器资源加载优化',type:'http://www.infoq.com/cn/articles/browser-resource-loading-optimization'},
+        {name:'浏览器是怎样工作的',url:'http://ued.ctrip.com/blog/how-browsers-work-rendering-engine-html-parsing-series-ii.html'},
+        {name:'浏览器资源加载优化',url:'http://www.infoq.com/cn/articles/browser-resource-loading-optimization'},
     ]
 }
